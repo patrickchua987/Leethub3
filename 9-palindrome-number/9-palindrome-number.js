cd104842f3976280da2831905322f7138f2974c0
patrickchua987 <51397988+patrickchua987@users.noreply.github.com>
2022-09-13 21:09:26 +0800

    Time: 346 ms (19.50%), Space: 50.8 MB (70.72%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -1,20 +1,14 @@
 /**
  * @param {number} x
  * @return {boolean}
- // strategy
- // recursive 
- // compare the first character vs the last characters
  */
-var isPalindrome = function(x,xx=x.toString()) {
-                   
-    if (xx.length ===0 || xx.length===1) return true;
-       
-    if (xx.charAt(0)===xx.charAt(xx.length-1)) {
-       if (xx.length===2) return true;
-        
-       return isPalindrome(parseInt(xx.substring(1,xx.length-1)),
-                          xx.substring(1,xx.length-1));
-    }
-    return false;
-      
+
+var isPalindrome = function(x) {
+
+let palindrome = x.toString().split("").reverse().join('');
+
+if(palindrome === x.toString()){
+   return true;
+}
+return false;
 };
