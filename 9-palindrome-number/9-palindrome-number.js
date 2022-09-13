25adcecc613cedbd1306d2fecccf35aaab0bbfd8
patrickchua987 <51397988+patrickchua987@users.noreply.github.com>
2022-09-13 19:54:48 +0800

    Time: 1342 ms (5.02%), Space: 64.1 MB (5.07%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -10,7 +10,7 @@ var isPalindrome = function(x,xx=x.toString()) {
     if (xx.length ===0 || xx.length===1) return true;
        
     if (xx.charAt(0)===xx.charAt(xx.length-1)) {
-      // if (xx.length===2) return true;
+       if (xx.length===2) return true;
         console.log('huli',xx.substring(1,xx.length-1),
                    parseInt(xx.substring(1,xx.length-1)));
        return isPalindrome(parseInt(xx.substring(1,xx.length-1)),
