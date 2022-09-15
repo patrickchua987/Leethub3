a208d79d0151ef3d5f2e670f2cbfd8ecd09ba4d5
patrickchua987 <51397988+patrickchua987@users.noreply.github.com>
2022-09-15 15:56:38 +0800

    Time: 92 ms (43.61%), Space: 42 MB (30.86%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -1,17 +1,3 @@
-/**
- * Definition for isBadVersion()
- * 
- * @param {integer} version number
- * @return {boolean} whether the version is bad
- * isBadVersion = function(version) {
- *     ...
- * };
- */
-
-/**
- * @param {function} isBadVersion()
- * @return {function}
- */
 var solution = function(isBadVersion) {
     /**
      * @param {integer} n Total versions
